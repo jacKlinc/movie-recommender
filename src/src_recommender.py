b111a205d804d37d36ad2f94a12073a18bed9c38
jacKlinc <jackharding321@gmail.com>
2021-01-16 17:20:39 +0100

    Now doesn't recommend movies that are not in the dataset.

--- src/src_recommender.py
+++ src/src_recommender.py
@@ -10,21 +10,22 @@ import streamlit as st
 
 '# Movie Recommender'
 "Here's the [GitHub](https://github.com/jacKlinc/movie_recommender) repo"
+'Type a movie with the year in brackets after. The movie you type might not be here'
 
-### Type favourite
+# Type favourite
 fav = st.text_input("Type a movie and hit Enter...")
 
-### Load model  
+# Load model
 learn = load_learner('./data/mdl_movie-recommend.pkl')
 
-### Load dataset
+# Load dataset
 path = untar_data(URLs.ML_100k)
 
 ratings = pd.read_csv(path/'u.data', delimiter='\t', header=None,
-                      names=['user','movie','rating','timestamp'])
+                      names=['user', 'movie', 'rating', 'timestamp'])
 
 movies = pd.read_csv(path/'u.item',  delimiter='|', encoding='latin-1',
-                     usecols=(0,1), names=('movie','title'), header=None)
+                     usecols=(0, 1), names=('movie', 'title'), header=None)
 
 # Join ratings and movies
 ratings = ratings.merge(movies)
@@ -32,18 +33,19 @@ ratings = ratings.merge(movies)
 # Add to DataLoaders object
 dls = CollabDataLoaders.from_df(ratings, item_name='title', bs=64)
 
-
-### Make embedding prediction
-movie_factors = learn.model.movie_factors
-idxs = dls.classes['title'].o2i[fav]
-# Finds distances from chosen title
-distances = nn.CosineSimilarity(dim=1)(movie_factors, movie_factors[idxs][None])
-# gets top 4
-idx = distances.argsort(descending=True)[1:4]
-
-
-# Recommend
 if fav:
-    '### You like: '
-    fav
-    '### You should watch: ', [dls.classes['title'][i] for i in idx]
+    if len(ratings[ratings.title == fav]) == 0:
+        "Can't find your movie, try another"
+    else:
+        # Make embedding prediction
+        movie_factors = learn.model.movie_factors
+        idxs = dls.classes['title'].o2i[fav]
+        # Finds distances from chosen title
+        distances = nn.CosineSimilarity(dim=1)(movie_factors, movie_factors[idxs][None])
+        # gets top 3
+        idx = distances.argsort(descending=True)[1:4]
+
+        # Recommend
+        '### You like: '
+        fav
+        '### You should watch: ', [dls.classes['title'][i] for i in idx]
